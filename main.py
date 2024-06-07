df21bc080ee6491cff0e4165154fff7a2bbe15ab
Ying Tong <yingtong0106@gmail.com>
2024-06-07 20:36:36 +0800

    Add SQLAlchemy database setup

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from models import db
+
+DATABASE_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = "beginnerz"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DATABASE_NAME}"
     
     @app.route("/")
     def index():
         return "Hello, World!"
+    
+    db.init_app()
+
+    with app.app_context():
+        db.create_all()
     
     return app
 
