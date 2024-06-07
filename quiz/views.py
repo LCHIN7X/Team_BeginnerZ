4f5e6c40c3556659ae75104263f4a39aa37d0ce7
Ying Tong <yingtong0106@gmail.com>
2024-06-07 21:26:35 +0800

    change url path

--- quiz/views.py
+++ quiz/views.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, render_template, redirect, url_for 
+from flask import Blueprint, render_template, redirect, url_for, request
 
 quiz = Blueprint('quiz',__name__,template_folder='templates',static_folder='static')
 
 
-@quiz.route('/take_quiz')
-def answer_quiz():
+@quiz.route('/take_quiz',methods=["GET","POST"])
+def quiz_page():
+    if request.method == "POST":
+        print("POST request successfully sent!")
+
+
     return render_template('quiz.html')
