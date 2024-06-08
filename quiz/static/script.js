e20d6cf8da34290b46e2e32bcf7a0a2f714bca18
Ying Tong <yingtong0106@gmail.com>
2024-06-08 10:08:01 +0800

    Question rendering works like intended: One question at a time, after user makes their choice, the second question shows up

--- quiz/static/script.js
+++ quiz/static/script.js
@@ -1,6 +1,9 @@
 const startQuizBtnEl = document.getElementById("start-quiz-btn-el");
 const quizContainerEl = document.getElementById('quiz-container-el')
 
+let currentQuestionIndex = 0
+let questions = []
+let score = 0
 
 startQuizBtnEl.addEventListener("click", (e) => {
   e.preventDefault()
@@ -8,25 +11,55 @@ startQuizBtnEl.addEventListener("click", (e) => {
     .then((res) => res.json())
     .then((data) => {
       quizContainerEl.innerHTML = ''
-      const questions = data.questions;
-      questions.forEach((question, index) => {
-        renderQuizQuestion(question, index);
-      });
+      questions = data.questions;
+      currentQuestionIndex = 0
+      startQuiz()
     })
     .catch(error => console.error('Error fetching questions:', error));
 });
 
 
-function renderQuizQuestion(question, index) {
+function startQuiz() {
+  // if (currentQuestionIndex < questions.length) {
+  //   const question = questions[currentQuestionIndex]
+  //   renderQuizQuestion(question)
+  // }
+  renderQuizQuestion()
+}
+
+
+function renderQuizQuestion() {
+  if (currentQuestionIndex < questions.length) {
+    const question = questions[currentQuestionIndex]
     quizContainerEl.innerHTML += `
     <div class="quiz-content">
-        <h2>Question ${index + 1}</h2>
+        <h2>Question ${currentQuestionIndex + 1}</h2>
         <p>${question.question}</p>
-        <p>${question.option_a}</p>
-        <p>${question.option_b}</p>
-        <p>${question.option_c}</p>
-        
-        <p>Correct Answer: ${question.answer}</p>
+        <button class='btn-primary btn' onclick="checkUserAnswer('${question.option_a}', '${question.answer}')">${question.option_a}</button>
+        <button class='btn-primary btn' onclick="checkUserAnswer('${question.option_b}', '${question.answer}')">${question.option_b}</button>
+        <button class='btn-primary btn' onclick="checkUserAnswer('${question.option_c}', '${question.answer}')">${question.option_c}</button>
     </div>
 `;
+  }
+  else {
+    quizContainerEl.innerHTML = `<h1>Quiz Complete!</h1>
+    <h2>Your score: ${score}/${questions.length}</h2>`
+  }
+}
+
+
+function checkUserAnswer(userSelection, correctAnswer) {
+  if (userSelection == correctAnswer) {
+    score ++
+    
+    console.log('Hooray! That is correct!')
+    console.log(score)
+  }
+  else {
+    console.log(userSelection)
+    console.log(correctAnswer)
+    console.log("Oops! Wrong Answer!")
+  }
+  currentQuestionIndex ++
+  renderQuizQuestion()
 }
